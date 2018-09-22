91b8be4a0aba1f1d382d7e52ee18f638c1762258
ZeroExistence <tonydeguia009@gmail.com>
2018-09-22 10:34:58 +0000

    Add git to initial installation and to clone the repository

--- debian-deploy-shadowsocks/initial-setup.sh
+++ debian-deploy-shadowsocks/initial-setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install python3-pip;
+apt-get update && apt-get install python3-pip git;
 pip3 install ansible;
+git clone https://github.com/ZeroExistence/server-setup-scripts.git;
+cd server-setup-scripts/debian-deploy-shadowsocks;
 ansible-playbook -C deploy.yml;
