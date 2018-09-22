64053072e64aa23b854af5221428830097770430
ZeroExistence <tonydeguia009@gmail.com>
2018-09-22 13:54:57 +0000

    Commiting the template for wireguard setup

--- debian-deploy-shadowsocks/initial-setup.sh
+++ debian-deploy-shadowsocks/initial-setup.sh
@@ -2,4 +2,4 @@
 apt-get -y update && apt-get -y install python3-pip git;
 pip3 install ansible;
 git clone https://github.com/ZeroExistence/server-setup-scripts.git;
-ansible-playbook server-setup-scripts/debian-deploy-shadowsocks/deploy.yml;
+# ansible-playbook server-setup-scripts/debian-deploy-shadowsocks/deploy.yml;
--- debian-initial-setup/initial-setup.sh
+++ debian-initial-setup/initial-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-apt-get update && apt-get install python3-pip;
+apt-get -y update && apt-get -y install python3-pip git;
 pip3 install ansible;
-ansible-playbook deploy.yml;
+git clone https://github.com/ZeroExistence/server-setup-scripts.git;
+ansible-playbook server-setup-scripts/debian-initial-setup/deploy.yml;
 # ip link add dev wg0 type wireguard;
 # ip addr add 192.168.100.11/28 dev wg0;
 # wg genkey | tee privatekey | wg pubkey > publickey;
