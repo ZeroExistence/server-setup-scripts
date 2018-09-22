60400c14557cb18fedf391edc96ec55b48f3c7f2
ZeroExistence <tonydeguia009@gmail.com>
2018-09-22 11:04:32 +0000

    Fixing the playbook and the initial-setup.sh

--- debian-deploy-shadowsocks/initial-setup.sh
+++ debian-deploy-shadowsocks/initial-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install python3-pip git;
+apt-get -y update && apt-get -y install python3-pip git;
 pip3 install ansible;
 git clone https://github.com/ZeroExistence/server-setup-scripts.git;
-cd server-setup-scripts/debian-deploy-shadowsocks;
-ansible-playbook -C deploy.yml;
+ansible-playbook server-setup-scripts/debian-deploy-shadowsocks/deploy.yml;
